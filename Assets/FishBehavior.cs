eaf9fb501d2ba4526e4e26113003a411f49f5b68
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-24 09:40:41 -0400

    Commit #1

---
+++ Assets/FishBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed=1f;
+    public bool dir; //right=true, left=false
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
