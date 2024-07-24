1e60a4f0c43955aa8b41040029057ae431dafad9
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-24 11:43:48 -0400

    Commit #3

--- Assets/FishBehavior.cs
+++ Assets/FishBehavior.cs
@@ -4,29 +4,52 @@ using UnityEngine;
 
 public class FishBehavior : MonoBehaviour
 {
-    public float speed = 3f;
-    public float rotationSpeed = 4f;
     public float boidRadius = 2.5f;
     public float separationWeight = 1.5f;
     public float alignmentWeight = 1f;
     public float cohesionWeight = 1f;
 
+    private List<GameObject> neighbors;
+    public float maxSpeed = 4;
+    public float steerStrength = 2;
+    public float wanderStrength = 1;
+
     private Rigidbody2D rb;
     private SpriteRenderer sr;
+
+    private Vector2 desiredDirection;
     void Start()
     {
+        desiredDirection = Vector2.zero;
         rb = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        desiredDirection = (desiredDirection + Random.insideUnitCircle * wanderStrength).normalized;
 
-        float angle = Random.Range(0, 360) * Mathf.Deg2Rad;
-        rb.velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * speed;
+        Vector2 desiredVelocity = desiredDirection * maxSpeed;
+        Vector2 desiredSteeringForce = (desiredVelocity - rb.velocity) * steerStrength;
 
-       
+        Vector2 acceleration = Vector2.ClampMagnitude(desiredSteeringForce, steerStrength) / 1;
+        rb.AddForce(acceleration, ForceMode2D.Force);
+
+        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeed);
+
+        float angle = Mathf.Atan2(rb.velocity.y, rb.velocity.x) * Mathf.Rad2Deg;
+        if (rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+        }
+        else if (rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+        }
     }
 
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        rb.velocity = -rb.velocity;
+    }
 }
