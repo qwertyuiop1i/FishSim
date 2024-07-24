781894e99f0c8ae117f5bb8710268f1d68005b34
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-24 10:42:44 -0400

    Commit #2

--- Assets/FishBehavior.cs
+++ Assets/FishBehavior.cs
@@ -4,18 +4,29 @@ using UnityEngine;
 
 public class FishBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float speed=1f;
-    public bool dir; //right=true, left=false
+    public float speed = 3f;
+    public float rotationSpeed = 4f;
+    public float boidRadius = 2.5f;
+    public float separationWeight = 1.5f;
+    public float alignmentWeight = 1f;
+    public float cohesionWeight = 1f;
 
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        float angle = Random.Range(0, 360) * Mathf.Deg2Rad;
+        rb.velocity = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)) * speed;
+
+       
     }
+
+
 }
