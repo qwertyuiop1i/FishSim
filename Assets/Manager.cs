86c79aaba099bbfc6d90c8730a304d680e84f7d1
qwertyuiop1i <66954603+qwertyuiop1i@users.noreply.github.com>
2024-07-25 13:30:06 -0400

    Commit #16

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    public int am;
+    public int[] tracks;
+
+    public float zoomedIn = 3.0f;
+    public float zoomedOut = 8.0f;
+    public Camera c;
+
     public Transform target;
     public float smoothSpeed = 0.125f;
 
@@ -20,13 +27,19 @@ public class Manager : MonoBehaviour
     public float t;
     public void Start()
     {
-        
+        c = GetComponent<Camera>();
     }
     public void changeVal(float v)
     {
         timeSpeed = v;
             }
-    public void Update()
+
+    public void spawn(GameObject spawnee)
+    {
+        Instantiate(spawnee);
+    }
+
+    private void Update()
     {
         Time.timeScale = timeSpeed;
         t += Time.deltaTime;
@@ -36,11 +49,35 @@ public class Manager : MonoBehaviour
             t = 0f;
 
         }
-    }
-    public void spawn(GameObject spawnee)
-    {
-        Instantiate(spawnee);
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("fish"))
+            {
+                
+                target = hit.transform;
+                following = !following;
+            }
+        }
+
+        if (following)
+        {
+            c.orthographicSize = zoomedIn;
+            FollowTarget();
+        }
+        else
+        {
+            c.orthographicSize = zoomedOut;
+            transform.position = new Vector3(0,0,-10);
+        }
     }
 
-    
+    private void FollowTarget()
+    {
+        Vector3 desiredPosition = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+    }
+
+    private Vector3 velocity = Vector3.zero;
 }
+
